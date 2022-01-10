c03d09730e7d6bc175db012c177d4b7cc18aa2a9
Alex Lofnes <alexlofnes@gmail.com>
2022-01-10 11:31:49 +0000

    first commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,9 @@
+<script lang="ts"></script>
+    import '../app.css';
+</script>
+  
+<main>
+    <p class="text-7xl text-red-500 bg-slate-600">Yo</p>
+    <h1 class="text-center">hi</h1>
+    <h2 class="bg-green-200">yo2</h2>
+</main>
